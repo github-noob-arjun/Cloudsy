7cb197713e869f072a53a7fb9772c03e22bfb037
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2023-07-05 13:56:38 +0530

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -167,13 +167,8 @@ async def pixmain(bot, msg):
             await msg.message.edit_text(text=f"Up Error :- `{error}`")    
             return upload.json()
     
-        try:
-            file_id = upload['id']
-            info = requests.get(f"https://pixeldrain.com/api/file/{file_id}/info")
-        except Exception as error:
-            await msg.message.edit_text(text=f"in Error :- `{error}`")
-            return info.json()
-
+        file_id = upload['id']
+        info = requests.get(f"https://pixeldrain.com/api/file/{file_id}/info")
         try:
             data = info(response["id"])
             Fname = data['name']
