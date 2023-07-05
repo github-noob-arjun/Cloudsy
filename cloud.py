5b1816f913ba6a94f2dc5214d8f8eea81ee48376
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2023-07-05 13:48:40 +0530

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -172,7 +172,7 @@ async def pixmain(bot, msg):
             data = requests.get(f"https://pixeldrain.com/api/file/{file_id}/info")
         except Exception as error:
             await msg.message.edit_text(text=f"in Error :- `{error}`")
-            return info.json()
+            return data.json()
 
         try:
             Fname = data['name']
